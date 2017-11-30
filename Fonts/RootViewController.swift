6e52f864ec5de8ecb9db7796bf1c71aa7e74c703
Kshitij Suthar <kshitijsuthar@Kshitijs-MacBook-Pro.local>
2017-11-29 20:50:49 -0500

    Linked RootViewController with storyboard

---
+++ Fonts/RootViewController.swift
@@ -0,0 +1,68 @@
+
+import UIKit
+
+class RootViewController: UITableViewController {
+    
+    private var familyNames: [String]!
+    private var cellPointSize: CGFloat!
+    private var favoritesList: FavoritesList!
+    private static let familyCell = "FamilyName"
+    private static let favoritesCell = "Favorites"
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        familyNames = (UIFont.familyNames as [String]).sorted()
+        
+        let preferredTableViewFonts = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        cellPointSize = preferredTableViewFonts.pointSize
+        favoritesList = FavoritesList.sharedFavoritesList
+        tableView.estimatedRowHeight = cellPointSize
+    }
+    
+    func fontForDisplay(atIndexPath indexPath: NSIndexPath) -> UIFont? {
+        
+        if indexPath.section == 0 {
+            
+            let familyName = familyNames[indexPath.row]
+            let fontName = UIFont.fontNames(forFamilyName: familyName).first
+            return fontName != nil ? UIFont(name: fontName!, size: cellPointSize) : nil
+        }
+        else{
+            return nil
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return favoritesList.favorites.isEmpty ? 1 : 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? familyNames.count : 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "All Font Families" : "My Favorite Fonts"
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0  {
+            // the font names list
+            let cell = tableView.dequeueReusableCell(withIdentifier: RootViewController.familyCell, for: indexPath)
+            cell.textLabel?.font = fontForDisplay(atIndexPath: indexPath as NSIndexPath)
+            cell.textLabel?.text = familyNames[indexPath.row]
+            cell.detailTextLabel?.text = familyNames[indexPath.row]
+            return cell
+        } else {
+            // the favourites list
+            return tableView.dequeueReusableCell(withIdentifier: RootViewController.favoritesCell, for: indexPath)
+        }
+        
+    }
+
+}
